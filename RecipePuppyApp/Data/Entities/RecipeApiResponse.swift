2d69dd750b00eb6c78a596cc7df58d4e2669272b
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2019-10-09 11:45:36 +0200

    Create Recipe Api Response

--- RecipePuppyApp/Data/Entities/RecipeApiResponse.swift
+++ RecipePuppyApp/Data/Entities/RecipeApiResponse.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct RecipeApiResponse {
+    let title: String
+    let version: Int
+    let href: String
+    let recipies: [RecipeEntity]
+}
+
+extension RecipeApiResponse: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case version
+        case href
+        case recipies = "results"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        title = try container.decode(String.self, forKey: .title)
+        version = try container.decode(Int.self, forKey: .version)
+        href = try container.decode(String.self, forKey: .href)
+        recipies = try container.decode([RecipeEntity].self, forKey: .recipies)
+    }
+}
