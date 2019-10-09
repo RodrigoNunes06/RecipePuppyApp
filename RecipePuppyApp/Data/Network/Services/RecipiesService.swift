31a543b9c9df8b1442bf697402e0631075e77808
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2019-10-09 11:46:05 +0200

    Create Recipe Service, Recipe Domain Entity

--- RecipePuppyApp/Data/Network/Services/RecipiesService.swift
+++ RecipePuppyApp/Data/Network/Services/RecipiesService.swift
@@ -8,10 +8,38 @@
 
 import Foundation
 
-final class RecipiesService: Service {
-    let router: NetworkRouter
+protocol RecipiesServiceApi {
+    func getRecipies(query: String,
+                     page: Int,
+                     completion: @escaping(Result<[RecipeEntity], Error>) -> Void)
+}
+
+final class RecipiesService: RecipiesServiceApi {
+    private let router: Router<RecipeEndPoint>
     
-    init(router: NetworkRouter) {
-        self.router = router as! Router<RecipeEndPoint>
+    init(router: Router<RecipeEndPoint>) {
+        self.router = router
+    }
+    
+    func getRecipies(query: String,
+                     page: Int,
+                     completion: @escaping(Result<[RecipeEntity], Error>) -> Void) {
+        router.request(.searchRecipe(query: query, page: page)) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            guard let responseData = data else {
+                completion(.failure(NetworkResponseError.noData))
+                return
+            }
+            
+            do {
+                let apiResponse = try JSONDecoder().decode(RecipeApiResponse.self, from: responseData)
+                completion(.success(apiResponse.recipies))
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
 }
--- RecipePuppyApp/Domain/Entities/Recipe.swift
+++ RecipePuppyApp/Domain/Entities/Recipe.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Recipe {
+    let title: String
+    let ingredients: String
+    let imageURL: String
+    
+    init(title: String = "", ingredients: String = "", imageURL: String = "") {
+        self.title = title
+        self.ingredients = ingredients
+        self.imageURL = imageURL
+    }
+}
