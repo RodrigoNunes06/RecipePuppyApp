1fdcc06b18b695ac6f2fd30e2e5bbd41fef95abe
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2019-10-10 15:58:05 +0200

    Rename GetRecipeUseCase

---
+++ RecipePuppyApp/Domain/UseCases/GetRecipeUseCase.swift
@@ -0,0 +1,27 @@
+//
+//  GetRecipeUseCase.swift
+//  RecipePuppyApp
+//
+//  Created by Rodrigo Nunes on 10/9/19.
+//  Copyright © 2019 Rodrigo Nunes Gil. All rights reserved.
+//
+
+import Foundation
+
+protocol GetRecipeUseCaseInterface {
+    func execute(recipe: String,
+    page: Int,
+    completion: @escaping RepositoryResult)
+}
+
+struct GetRecipeUseCase: GetRecipeUseCaseInterface {
+    private let repository = RecipeRepositoryImpl()
+    
+    func execute(recipe: String,
+                 page: Int,
+                 completion: @escaping RepositoryResult) {
+        repository.getRecipes(recipe: recipe, page: page) { result in
+            completion(result)
+        }
+    }
+}
