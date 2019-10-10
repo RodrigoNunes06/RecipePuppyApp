66b2bbbba6ea5fe87c6077ea8fcb3b50c45ee03f
Rodrigo Nunes <rodrigo.nunes06@gmail.com>
2019-10-10 21:05:57 +0200

    Add GetFavoriteRecipeUseCase

---
+++ RecipePuppyApp/Domain/UseCases/GetFavoriteRecipesUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  GetFavoriteRecipesUseCase.swift
+//  RecipePuppyApp
+//
+//  Created by Rodrigo Nunes on 10/10/19.
+//  Copyright © 2019 Rodrigo Nunes Gil. All rights reserved.
+//
+
+import Foundation
+
+protocol GetFavoriteRecipesUseCaseInterface {
+    func execute() -> [Recipe]
+}
+
+struct GetFavoriteRecipesUseCase: GetFavoriteRecipesUseCaseInterface {
+    private let repository = RecipeRepositoryImpl()
+    
+    func execute() -> [Recipe] {
+        return repository.getFavoriteRecipes()
+    }
+}
